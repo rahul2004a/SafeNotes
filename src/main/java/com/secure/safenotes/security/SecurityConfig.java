92d18ba59cf34bd28becc988bc5e7b11ac3cd8b2
rahul2004a <kumar.rahul.2017354@gmail.com>
2024-11-27 00:28:00 +0530

    update spring security , integrate sql and check each endpoint

--- src/main/java/com/secure/safenotes/security/SecurityConfig.java
+++ src/main/java/com/secure/safenotes/security/SecurityConfig.java
@@ -2,28 +2,22 @@ package com.secure.safenotes.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> {
-            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl) requests
-                    .requestMatchers("/contact").permitAll()
-                    .requestMatchers("/admin").denyAll()
-                    .anyRequest()).authenticated();
-        });
-//        http.formLogin(Customizer.withDefaults());
-        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.httpBasic(Customizer.withDefaults());
-        return (SecurityFilterChain)http.build();
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.csrf(AbstractHttpConfigurer::disable);
+        //http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
     }
 }
