d26ef15fa7660d8a423e03cdd00433d8b0ff3ba8
rahul2004a <kumar.rahul.2017354@gmail.com>
2024-12-04 20:03:04 +0530

    create admin controller, service and repository

---
+++ src/main/java/com/secure/safenotes/services/UserService.java
@@ -0,0 +1,14 @@
+package com.secure.safenotes.services;
+
+import com.secure.safenotes.dtos.UserDTO;
+import com.secure.safenotes.models.User;
+
+import java.util.List;
+
+public interface UserService {
+    void updateUserRole(Long userId, String roleName);
+
+    List<User> getAllUsers();
+
+    UserDTO getUserById(Long id);
+}
