52667fdc7d99845e2dbba2346f8f8a1f4321da0e
rahul2004a <kumar.rahul.2017354@gmail.com>
2024-12-08 17:43:11 +0530

    Implementing AuditLog Controller and Recording Audit Event

--- src/main/java/com/secure/safenotes/repositories/AuditLogRepository.java
+++ src/main/java/com/secure/safenotes/repositories/AuditLogRepository.java
@@ -3,5 +3,8 @@ package com.secure.safenotes.repositories;
 import com.secure.safenotes.models.AuditLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+    List<AuditLog> findByNoteId(Long id);
 }
--- src/main/java/com/secure/safenotes/services/AuditLogService.java
+++ src/main/java/com/secure/safenotes/services/AuditLogService.java
@@ -1,6 +1,9 @@
 package com.secure.safenotes.services;
 
+import com.secure.safenotes.models.AuditLog;
 import com.secure.safenotes.models.Note;
+
+import java.util.List;
 
 public interface AuditLogService {
     void logNoteCreation(String username, Note note);
@@ -8,4 +11,8 @@ public interface AuditLogService {
     void logNoteUpdate(String username, Note note);
 
     void logNoteDeletion(String username, Long noteId);
+
+    List<AuditLog> getAllAuditLogs();
+
+    List<AuditLog> getAuditLogsForNoteId(Long id);
 }
